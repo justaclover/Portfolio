c1ca876804d24d355dd5be33ae10193383280e48
justaclover <jane.thecat2308@gmail.com>
2025-02-02 18:31:44 +0300

    Добавлена работа с анкетой

---
+++ abiturient/abiturient.js
@@ -0,0 +1,19 @@
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation');
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
